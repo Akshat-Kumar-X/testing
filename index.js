4168cbf43482872df5c7140f3b6e2ab93f26ff1e
unknown <akshatkumar10203@gmail.com>
2023-08-20 00:40:21 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", ( request, response ) => {
+    response.send("Home");
+});
+
+app.get("/about", ( request, response) => {
+    response.send("About");
+});
+
+app.listen(0, ( request, response ) => {
+    console.log("Server Started")
+});
